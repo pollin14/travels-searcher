eb8adff5593cd767783ee03ebe3e7e9b047d0d48
Victor Aguilar <victor.aguilar@clickbus.com.mx>
2015-11-20 16:20:23 -0600

    Created scafolding to angular component.

---
+++ app/scripts/app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name searchWidgetApp
+ * @description
+ * # searchWidgetApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('searchWidgetApp', [
+    'ngCookies',
+    'ngSanitize',
+    'ngTouch'
+  ]);
