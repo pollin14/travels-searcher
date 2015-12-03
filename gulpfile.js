c09cb6fddbe5a7ffc4b6d831152dbb32479ac22c
Victor Aguilar <victor.aguilar@clickbus.com.mx>
2015-12-03 16:48:57 -0600

    Added watch task.

--- gulpfile.js
+++ gulpfile.js
@@ -7,8 +7,19 @@ const wrap              = require('gulp-wrap');
 const replace           = require('gulp-replace');
 const pack              = require('./package.json');
 
-gulp.task('build-dev', ['build-dist'], function () {
-    gulp.src('dist/**/*')
+var wrappingFunction = '(function(){\n\'use strict\';\n<%= contents %>\n})();';
+
+gulp.task('watch', ['build-dev'], function () {
+    gulp.watch('src/**/*.js', ['build-dev']);
+});
+
+gulp.task('build-dev', function () {
+    gulp.src('src/scripts/**/*.js')
+        .pipe(sourcemaps.init())
+        .pipe(replace(/("|')use strict\1;*/g, ''))
+        .pipe(concat(pack.name + '.js'))
+        .pipe(wrap(wrappingFunction))
+        .pipe(sourcemaps.write())
         .pipe(gulp.dest('public'));
 });
 
@@ -18,13 +29,12 @@ gulp.task('build-dist', function () {
             .pipe(replace(/("|')use strict\1;*/g, ''))
             .pipe(uglify())
             .pipe(concat(pack.name + '.js'))
-            .pipe(wrap('(function(){\n"use strict";\n<%= contents %>\n})();'))
+            .pipe(wrap(wrappingFunction))
         .pipe(sourcemaps.write())
         .pipe(gulp.dest('dist'));
 });
 
-
-gulp.task('serve-dev', function () {
+gulp.task('serve-dev', ['watch'], function () {
     gulp.src(['build', 'public'])
         .pipe(webserver({
             livereload: true,
