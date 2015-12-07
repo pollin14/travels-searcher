733d1088d183c86b824e8d4288305b01055bf9e3
Victor Aguilar <victor.aguilar@clickbus.com.mx>
2015-12-07 17:53:58 -0600

    Added translation file to validations

---
+++ src/translations/validationsTranslations.es.js
@@ -0,0 +1,21 @@
+(function () {
+    'use strict';
+
+    window.validationsTranslations = {
+        origin: {
+            required: {
+                message: 'Ingrese el lugar de origen.'
+            }
+        },
+        destination: {
+            required: {
+                message: 'Ingrese el lugar de destino.'
+            }
+        },
+        departureDate: {
+            required: {
+                message: 'Seleccione una fecha.'
+            }
+        }
+    };
+})();
