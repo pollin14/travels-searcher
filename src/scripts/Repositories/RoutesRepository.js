722558574e4eed41e985b5d0ab1d88fd335ab19c
Victor Aguilar <victor.aguilar@clickbus.com.mx>
2015-12-09 16:13:44 -0600

    Implemented the method findByOrigin.

--- src/scripts/Repositories/RoutesRepository.js
+++ src/scripts/Repositories/RoutesRepository.js
@@ -1,24 +1,33 @@
-'use strict';
+(function ($) {
+    'use strict';
 
-/**
- *
- * @constructor
- */
-var RoutesRepository = function () {
+    /**
+     *
+     * @constructor
+     */
+    var RoutesRepository = function (placeRepository, url) {
+        this.url = url;
+        this.placeRepository = placeRepository;
+    };
 
-};
+    /**
+     *
+     * @param origin
+     * @returns {*}
+     */
+    RoutesRepository.prototype.findByOrigin = function (origin) {
+        var that = this;
 
-RoutesRepository.prototype.findByOrigin = function (origin) {
+        return $
+            .getJSON(this.url, {placeId: origin.id})
+            .then(function (adjacencyList) {
+                var ids = adjacencyList.map(function (item) {
+                    return item.arrival;
+                });
 
-    return [
-        {
-            name: 'Name return Return',
-            value: 'Ciudad value Return seachable',
-            slug: 'ciudad-de-mexico-return',
-            priority: 0,
-            city: 'México Return',
-            terminal: 'Todas las return ',
-            state: 'DX'
-        }
-    ];
-};
+                return that.placeRepository.findByIds(ids);
+            });
+    };
+
+    window.RoutesRepository = RoutesRepository;
+})(jQuery);
