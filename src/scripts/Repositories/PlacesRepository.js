da395b9e43a553a1775e9fdeba5ab0310f2b9fbd
Victor Aguilar <victor.aguilar@clickbus.com.mx>
2015-12-09 16:12:09 -0600

    Added the findByIds method.

--- src/scripts/Repositories/PlacesRepository.js
+++ src/scripts/Repositories/PlacesRepository.js
@@ -1,39 +1,58 @@
-'use strict';
+/* globals placesRepositoryData */
 
-/**
- * @constructor
- */
-var PlacesRepository = function () {
+(function () {
+    'use strict';
 
-};
+    /**
+     * @constructor
+     */
+    var PlacesRepository = function () {
+        this.places = [];
 
-/**
- * @param {bool} enabledPagination (optional, default:true)
- * @return a collection of object with the next properties.
- * <code>
- *     {
- *          name: "The place name"
- *          value: "This field was use to do the search",
- *          slug: "slug of the place",
- *          priority: "Integer Positive. More big more priority"
- *          city: "City name"
- *          terminal: "Terminal name"
- *     }
- * </code>
- */
-PlacesRepository.prototype.findAll = function (enabledPagination) {
-
-    enabledPagination = typeof enabledPagination === 'boolean'? enabledPagination: true;
-
-    return [
-        {
-            name: 'Ciudad de México',
-            value: 'Ciudad de México seachable',
-            slug: 'ciudad-de-mexico-todas-las-terminales',
-            priority: 0,
-            city: 'México',
-            terminal: 'Todas las terminales',
-            state: 'DF'
+        if (typeof placesRepositoryData !== 'undefined') {
+            this.places = placesRepositoryData;
+        } else {
+            if (typeof console !== 'undefined') {
+                console.log('The places are empties. Maybe you forgot to add the placesRepositoryData.');
+            }
         }
-    ];
-};
+    };
+
+    /**
+     * @param {bool} enabledPagination (optional, default:true)
+     * @return a collection of object with the next properties.
+     * <code>
+     *     {
+     *          id: 123
+     *          name: "The place name",
+     *          value: "This field was use to do the search",
+     *          slug: "slug of the place",
+     *          priority: "Integer Positive. More big more priority",
+     *          isGroup: true,
+     *          city: "Raccoon City",
+     *          terminal: "Terminal name",
+     *          state: "DF"
+     *     }
+     * </code>
+     */
+    PlacesRepository.prototype.findAll = function (enabledPagination) {
+
+        enabledPagination = typeof enabledPagination === 'boolean'? enabledPagination: true;
+
+        return this.places;
+    };
+
+    /**
+     *
+     * @param ids
+     * @returns {Array.<id>}
+     */
+    PlacesRepository.prototype.findByIds = function (ids) {
+
+        return this.places.filter(function (item) {
+            return ids.includes(item.id);
+        });
+    };
+
+    window.PlacesRepository = PlacesRepository;
+})();
