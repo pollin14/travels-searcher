3f195d7a649ea20fce2d8407018ea8882c3a9652
Victor Aguilar <victor.aguilar@clickbus.com.mx>
2015-12-10 18:42:22 -0600

    Updated demo.

--- demo/main.js
+++ demo/main.js
@@ -1,4 +1,5 @@
 
 $('form.dummy-travel-searcher-form').travelsSearcher({
-    adjacencyListUlr: '/demo/adjacency-list.json'
+    adjacencyListUlr: '/demo/adjacency-list.json',
+    resultsAmount: 20
 });
