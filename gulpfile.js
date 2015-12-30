8249bb2e8cd839a6de69ca03134d89b86e008bff
Victor Aguilar <victor.aguilar@clickbus.com.mx>
2015-12-30 10:20:13 -0600

    Removed wrapper function

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,9 @@ const webserver         = require('gulp-webserver');
 const concat            = require('gulp-concat');
 const uglify            = require('gulp-uglify');
 const sourcemaps        = require('gulp-sourcemaps');
-const wrap              = require('gulp-wrap');
 const replace           = require('gulp-replace');
 const minifyCss         = require('gulp-minify-css');
 const pack              = require('./package.json');
-
-var wrappingFunction = '(function(){\n\'use strict\';\n<%= contents %>\n})();';
 
 gulp.task('watch', ['build-dev'], function () {
     gulp.watch(['src/scripts/**/*.js', 'src/stylesheets/*.css'], ['build-dev']);
@@ -24,19 +21,15 @@ gulp.task('compile-stylesheets', function () {
 
 gulp.task('compile-scripts', function () {
     gulp.src('src/scripts/**/*.js')
-        .pipe(replace(/("|')use strict\1;*/g, ''))
         .pipe(concat(pack.name + '.js'))
-        .pipe(wrap(wrappingFunction))
         .pipe(gulp.dest('dist'));
 });
 
 gulp.task('build-dist', function () {
     gulp.src('src/scripts/**/*.js')
         .pipe(sourcemaps.init())
-            .pipe(replace(/("|')use strict\1;*/g, ''))
             .pipe(uglify())
             .pipe(concat(pack.name + '.js'))
-            .pipe(wrap(wrappingFunction))
         .pipe(sourcemaps.write())
         .pipe(gulp.dest('dist'));
 });
