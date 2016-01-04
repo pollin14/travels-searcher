c17be6c3acb5ad5a84582c268f3e77409070e7dc
Victor Aguilar <victor.aguilar@clickbus.com.mx>
2016-01-04 16:21:39 -0600

    Updated the demo with a custome event listener

--- demo/main.js
+++ demo/main.js
@@ -3,3 +3,8 @@ $('form.dummy-travel-searcher-form').travelsSearcher({
     adjacencyListUlr: '/demo/adjacency-list.json',
     resultsAmount: 20
 });
+
+
+$(document).on('clickbus.travels-searcher-submit', function () {
+   alert('The form is valid.');
+});
